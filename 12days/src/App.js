e0c2233ee8cf363f65c7f9e6bb4e31b0ba8e4472
teochewthunder <teochewthunder@gmail.com>
2020-10-17 15:58:33 +0800

    Basic structure

--- 12days/src/App.js
+++ 12days/src/App.js
@@ -1,12 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Row from './components/Row';
 
 function App() {
-  return (
-    <div className="App">
+	const [currentDay, setCurrentDay] = useState(1);
 
-    </div>
-  );
+    const daySuffix = [
+    	"st",
+    	"nd",
+    	"rd",
+    	"th",
+    	"th",
+    	"th",
+    	"th",
+    	"th",
+    	"th",
+    	"th",
+    	"th",
+    	"th"
+    ]
+
+    const BtnClickUp = () => {
+    	if (currentDay < 12) {
+    		let toDay = currentDay;
+    		setCurrentDay(0);
+
+    		setTimeout(() => {
+    				setCurrentDay(toDay + 1);
+    			},
+    			500
+    		);   		
+    	}
+    }
+
+    const BtnClickDown = () => {
+    	if (currentDay > 1) {
+    		let toDay = currentDay;
+    		setCurrentDay(0);
+    		setTimeout(() => {
+    				setCurrentDay(toDay - 1);
+    			},
+    			500
+    		);        	
+    	}    	
+    }
+
+    const dayControls = (
+    	<div className="dayControl">
+    		<div className="dayText">
+    		{ currentDay == 0 ? '' : currentDay }
+    		<sup> 
+    			{ currentDay == 0 ? '' : daySuffix[currentDay - 1]  }
+    		</sup>
+    		</div>
+
+    		<div className="dayButtons">
+    			<div className="dayButton" onClick={ BtnClickUp }>^</div>
+    			<div className="dayButton" onClick={ BtnClickDown }>^</div>
+    		</div>
+    	</div>
+    );
+
+    return (
+        <div className="App">
+        	<h1>On the { dayControls } day of Christmas my true love gave to me</h1>
+            <Row currentDay = {currentDay} day="12" />
+            <Row currentDay = {currentDay} day="11" />
+            <Row currentDay = {currentDay} day="10" />
+            <Row currentDay = {currentDay} day="9" />
+            <Row currentDay = {currentDay} day="8" />
+            <Row currentDay = {currentDay} day="7" />
+            <Row currentDay = {currentDay} day="6" />
+            <Row currentDay = {currentDay} day="5" />
+            <Row currentDay = {currentDay} day="4" />
+            <Row currentDay = {currentDay} day="3" />
+            <Row currentDay = {currentDay} day="2" />
+            <Row currentDay = {currentDay} day="1" />           
+        </div>
+    );
 }
 
 export default App;
---
+++ 12days/src/components/Row/Row.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import day1 from '../../svg/day1.svg';
+import day2 from '../../svg/day2.svg';
+import day3 from '../../svg/day3.svg';
+import day4 from '../../svg/day4.svg';
+import day5 from '../../svg/day5.svg';
+import day6 from '../../svg/day6.svg';
+import day7 from '../../svg/day7.svg';
+import day8 from '../../svg/day8.svg';
+import day9 from '../../svg/day9.svg';
+import day10 from '../../svg/day10.svg';
+import day11 from '../../svg/day11.svg';
+import day12 from '../../svg/day12.svg';
+import './Row.css';
+
+function Row(props) {
+    let day = props.day;
+    let currentDay = props.currentDay;
+
+    let content = [
+        { text: '', file: '' },
+        { text: 'a partridge in a pear tree!', file: day1 },
+        { text: 'two turtle doves, and', file: day2 },
+        { text: 'three french hens', file: day3 },
+        { text: 'four calling birds', file: day4 },
+        { text: 'five gold rings', file: day5 },
+        { text: 'six swans a-swimming', file: day6},
+        { text: 'seven geese a-laying', file: day7 },
+        { text: 'eight maids a-milking', file: day8 },
+        { text: 'nine ladies dancing', file: day9 },
+        { text: 'ten lords a-leaping', file: day10 },
+        { text: 'eleven pipers piping', file: day11 },
+        { text: 'twelve drummers drumming', file: day12 }
+    ];
+
+    let arr = [];
+    for (let i = 0; i < day; i++) arr.push('');
+
+    const images = arr.map((item, index) => (
+            <img key={ day+'img'+index } src={ content[day].file } width={ day >= 6 ? '100' : '200'} height={ day >= 6 ? '100' : '200'} />
+        )
+    );
+       
+    return (
+        <div className={ 'Row' + (currentDay >= day ? '' : ' Hidden') } style={{transition:'all ' + (currentDay == 0 ? 0 : (13 - day) * 0.5) + 's'}}>
+            <h1>{ content[day].text }</h1>
+            { images }
+        </div>
+    );
+}
+
+export default Row;
